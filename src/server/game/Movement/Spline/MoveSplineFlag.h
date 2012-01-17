ac837dfbf8753883fa49ed5f2ba2a5c7e2e60a46
Venugh <venugh@gmx.net>
2012-01-17 22:25:11 +0100

    Update flags, anything is wrong here, because flighmasters works not more.

--- src/server/game/Movement/Spline/MoveSplineFlag.h
+++ src/server/game/Movement/Spline/MoveSplineFlag.h
@@ -49,15 +49,15 @@ namespace Movement
             Catmullrom   = 0x00040000,           // Used Catmullrom interpolation mode
             Cyclic       = 0x00080000,           // Movement by cycled spline
             Enter_Cycle  = 0x00100000,           // Everytimes appears with cyclic flag in monster move packet, erases first spline vertex after first cycle done
-            Animation    = 0x00200000,           // Plays animation after some time passed
+            AnimationTier  = 0x00200000,           // Plays animation after some time passed
             Frozen       = 0x00400000,           // Will never arrive
-            Unknown5     = 0x00800000,
-            Unknown6     = 0x01000000,
+            Transport    = 0x00800000,
+            Exit_Vehicle = 0x01000000,
             Unknown7     = 0x02000000,
             Unknown8     = 0x04000000,
             OrientationInversed = 0x08000000,
-            Unknown10    = 0x10000000,
-            Unknown11    = 0x20000000,
+            UsePathSmoothing    = 0x10000000,
+            Animation    = 0x20000000,
             Unknown12    = 0x40000000,
             Unknown13    = 0x80000000,
 
@@ -69,8 +69,8 @@ namespace Movement
             Mask_No_Monster_Move = Mask_Final_Facing | Mask_Animations | Done,
             // CatmullRom interpolation mode used
             Mask_CatmullRom = Flying | Catmullrom,
-            // Unused, not suported flags
-            Mask_Unused = No_Spline|Enter_Cycle|Frozen|Unknown5|Unknown6|Unknown7|Unknown8|Unknown10|Unknown11|Unknown12|Unknown13,
+            // Unused, not supported flags
+            Mask_Unused = No_Spline|Enter_Cycle|Frozen|Transport|Exit_Vehicle|Unknown7|Unknown8|UsePathSmoothing|Animation|Unknown12|Unknown13,
         };
 
         inline uint32& raw() { return (uint32&)*this;}
